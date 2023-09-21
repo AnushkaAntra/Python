ce953a9accec4ce3efb3f2d59826d9e75665b006
Anushka Antra <116517571+AnushkaAntra@users.noreply.github.com>
2023-09-21 21:58:28 +0530

    Create First_programme.py

---
+++ First_programme.py
@@ -0,0 +1,17 @@
+n = int(input("How many numbers?"))
+
+n1, n2 = 0, 1
+count = 0
+
+if n<=0
+   print("please enter the positive number")
+elif n==1
+   print("Fibonacci sequence upto",n,":",n1)
+else:
+   print("Fibonacci sequence is: ")
+   while count < n
+        print n1
+        nth = n1+n2
+        n1 = n2
+        n2 = nth
+        count += 1
